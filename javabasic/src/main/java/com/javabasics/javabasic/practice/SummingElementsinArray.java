22f751d40e3a778857f1f5cfc033d48e81aad125
nidhichoudhary8 <87360770+nidhichoudhary8@users.noreply.github.com>
2021-07-21 09:48:01 -0700

    Update SummingElementsinArray.java

--- javabasic/src/main/java/com/javabasics/javabasic/practice/SummingElementsinArray.java
+++ javabasic/src/main/java/com/javabasics/javabasic/practice/SummingElementsinArray.java
@@ -7,5 +7,15 @@ package com.javabasics.javabasic.practice;
  *
  */
 public class SummingElementsinArray {
-
+public static void main(String[] args) {
+	int[] myList = {3, 4, 1, 2,5,6};
+	int sum=0;
+	
+	for(int i=0;i<myList.length;i++)
+	{
+		sum= sum + myList[i];
+	}
+	System.out.println(sum);
+}
+}
 }
