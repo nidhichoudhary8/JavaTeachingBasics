a0f350d7ab196dc5031491f9f564c89a230b175e
nidhichoudhary8 <87360770+nidhichoudhary8@users.noreply.github.com>
2021-07-21 09:37:00 -0700

    Update LargetsElementInArray.java

--- javabasic/src/main/java/com/javabasics/javabasic/practice/LargetsElementInArray.java
+++ javabasic/src/main/java/com/javabasics/javabasic/practice/LargetsElementInArray.java
@@ -7,5 +7,29 @@ package com.javabasics.javabasic.practice;
  *
  */
 public class LargetsElementInArray {
-
+public static void main(String[] args) {
+	int[] myList = {3, 4, 1, 2,5,6};
+	
+	for(int i=0;i<myList.length;i++)
+	{
+		for (int j=i+1;j<myList.length;j++)
+		{
+			int temp=0;
+			if (myList[i]<myList[j])
+			{
+	
+				temp =myList[i];
+				myList[i]=myList[j];
+				myList [j]=temp;
+				
+				
+			}
+		}
+	
+	System.out.println(myList[0]);
+	break;
+	
+}
+}
+}
 }
