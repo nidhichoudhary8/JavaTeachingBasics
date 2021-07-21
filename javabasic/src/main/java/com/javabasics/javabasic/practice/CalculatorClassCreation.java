ccb0ac8555ceca240bf13a7521ad2d28687a7a1a
nidhichoudhary8 <87360770+nidhichoudhary8@users.noreply.github.com>
2021-07-21 08:17:01 -0700

    Update CalculatorClassCreation.java

--- javabasic/src/main/java/com/javabasics/javabasic/practice/CalculatorClassCreation.java
+++ javabasic/src/main/java/com/javabasics/javabasic/practice/CalculatorClassCreation.java
@@ -1,5 +1,52 @@
 package com.javabasics.javabasic.practice;
-
+import java.util.Scanner;
 public class CalculatorClassCreation {
+  public static void main(String[] args)
+Scanner scan = new Scanner(System.in);
+		System.out.println(" enter your choice");
+		String abc = scan.nextLine();
+		System.out.println("enter the numbers");
+		int a = scan.nextInt();
+		int b = scan.nextInt();
+		
+		
+		
+		switch(abc)
+		{
+		case  "+" :
+			int sum = a+b;
+			System.out.println(sum);
+			break;
+			
+		case  "-" :
+			int diff = a-b;
+			System.out.println(diff);
+			break;
+			
+		case  "*" :
+			int multiply = a*b;
+			System.out.println(multiply);
+			break;
+			
+		case  "/" :
+			int divide = a/b;
+			System.out.println(divide);
+			break;
+			
+			default :
+				System.out.println("invalid");
+			
+			
+			
+			
+			
+			
+		}
+	
+		
+		
+	}
+
+}
 
 }
