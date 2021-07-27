df1dc316b1abd138cdf093f3cf2b186f1014d741
nidhichoudhary8 <87360770+nidhichoudhary8@users.noreply.github.com>
2021-07-27 07:13:18 -0700

    Update NumberOfVowles.java

--- javabasic/src/main/java/com/javabasics/javabasic/practice/NumberOfVowles.java
+++ javabasic/src/main/java/com/javabasics/javabasic/practice/NumberOfVowles.java
@@ -11,7 +11,26 @@ package com.javabasics.javabasic.practice;
 public class NumberOfVowles {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+	package training.java.hackerrank;
+
+public class Largest_in_array {
+public static void main(String[] args) {
+ String s = "Hello how are you";
+ int count =0;
+ 
+ for(int i=0;i<s.length();i++)
+ {
+	 
+ if (s.charAt (i)=='a'||s.charAt (i)=='e'||s.charAt (i)=='i'||s.charAt (i)=='o'||s.charAt (i)=='u')
+ {
+	 count++;
+	 
+ }
+ }
+ 
+ System.out.println("number of vowels is"+count);
+}
+}
 
 	}
 
